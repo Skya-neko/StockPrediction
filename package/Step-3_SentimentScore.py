03432d22dc11773c4cd1b357458c22d4fd42fe23
Lalalin <daby6789@gmail.com>
2022-06-10 11:55:06 +0800

    revise & run: Step-3_SentimentScore.py deduct_antiTSMC_score and make sure {台積電, 三星} only be counted once.

--- package/Step-3_SentimentScore.py
+++ package/Step-3_SentimentScore.py
@@ -61,6 +61,12 @@ def sum_category_score(categoryList,category):
     stockCodeDF = pd.read_csv('./data/Step-3_StockCodeList.csv')
     categoryIdx = stockCodeDF['stock_name'].isin(categoryList)
     categoryInfoDF = stockCodeDF[categoryIdx]
+    if category == 'TSMC':
+        TSMCMask1 = categoryInfoDF['stock_name'].isin(['台積電'])
+        TSMCMask2 = categoryInfoDF['industry_category'].isin(['電子工業'])
+        TSMCMask = TSMCMask1 & TSMCMask2
+        TSMCMaskIdx = TSMCMask[TSMCMask].index
+        categoryInfoDF = categoryInfoDF.drop(TSMCMaskIdx)
     categoryInfoDF.reset_index(drop=True,inplace=True)
     for i_keyword in range(len(categoryInfoDF)):
         industry = categoryInfoDF['industry_category'][i_keyword]
@@ -71,8 +77,7 @@ def sum_category_score(categoryList,category):
         for i_sentence in range(len(sentenceDF)):
             t0 = time.time()
             
-            timestamp = sentenceDF['PublishDate'][i_sentence][:10]
-            timestampList.append(timestamp)
+
 
             sentence = sentenceDF['Sentence'][i_sentence]
             ws_result = ws([sentence],                                   # Segment this sentence into words
@@ -80,10 +85,19 @@ def sum_category_score(categoryList,category):
                            recommend_dictionary=recommend_dictionary,
                            coerce_dictionary=coerce_dictionary,
                            )
+            # if 台積電, 三星 are both in the sentence at the same time,
+            # when it's turn to count 台積電 then count score,
+            # when it's turn to count 三星 then ignore the sentence.
+            if category == 'antiTSMC' and '台積電' in ws_result[0] and '三星' in ws_result[0]:
+                print('continue')
+                continue
             posIdx = positiveDf['positive'].isin(ws_result[0])           # Find out positive words index
             negIdx = negativeDf['negative'].isin(ws_result[0])           # Find out negative words index
             score = posIdx.sum() - negIdx.sum()                          # Calculate sentiment score
             scoreList.append(score)
+
+            timestamp = sentenceDF['PublishDate'][i_sentence][:10]
+            timestampList.append(timestamp)
 
             t1 = time.time()
 
@@ -117,25 +131,63 @@ def sum_category_score(categoryList,category):
     allKeywordScoreDF.to_csv(f'./data/Step-2_CategorySentimentScore/Score_{category}.csv', index=False)
 
 
+def remove_dates_beyond(category, startDate, endDate):
+    startDate = datetime.strptime(startDate, '%Y-%m-%d')
+    endDate = datetime.strptime(endDate, '%Y-%m-%d')
+    targetDF = pd.read_csv(f'./data/Step-2_CategorySentimentScore/Score_{category}.csv')
+    targetDF['PublishDate'] = pd.to_datetime(targetDF['PublishDate'])
+    mask = (targetDF['PublishDate'] > startDate) & (targetDF['PublishDate'] < endDate)
+    targetDF = targetDF.loc[mask]
+    # targetDF.to_csv(f'./data/Step-2_CategorySentimentScore/Score_{category}.csv', index=False)
 
-def sum_anti_score(categoryA, categoryB):
+
+def deduct_antiTSMC_score(categoryA, categoryB):
     categoryADF = pd.read_csv(f'./data/Step-2_CategorySentimentScore/Score_{categoryA}.csv')
     categoryBDF = pd.read_csv(f'./data/Step-2_CategorySentimentScore/Score_{categoryB}.csv')
     categoryADF['Anti-Score'] = categoryBDF['Score']
     categoryADF['FinalScore'] = categoryADF['Score'] - categoryADF['Anti-Score']
     categoryADF['Score'] = categoryADF['FinalScore']
     # categoryADF.drop(['Anti-Score', 'FinalScore'], axis=1)
-    categoryADF.to_csv(f'./data/Step-2_CategorySentimentScore/Score_{categoryA}.csv', index=False)
+    categoryADF.to_csv(f'./data/Step-2_CategorySentimentScore/Score_{categoryA}Mix.csv', index=False)
+
+def sum_all_score(categoryList):
+    categoryADF = pd.DataFrame()
+    for i_category in categoryList:
+        if categoryADF.empty:
+            categoryADF = pd.read_csv(f'./data/Step-2_CategorySentimentScore/Score_{i_category}.csv')
+        else:
+            categoryBDF = pd.read_csv(f'./data/Step-2_CategorySentimentScore/Score_{i_category}.csv')
+            categoryADF[f'{i_category}-Score'] = categoryBDF['Score']
+    categoryADF['Score'] = categoryADF.sum(axis=1)
+    categoryADF = categoryADF[['PublishDate', 'Score']]
+    categoryADF.to_csv(f'./data/Step-2_CategorySentimentScore/Step-2_SentimentScore.csv', index=False)
+
+
+
+
+
+
 
 
 # Category lists
 marketKeyword = ['台股', '大盤', '外資', '投信', '自營商', '法人', '加權指數', '美股', '台灣', '美國', '景氣', ]
+# the score of 台積電 will be doubled, because there are two 台積電 in the stockCodeDF
 TSMCKeyword = ['半導體', '電子', '晶圓', '台積電', '奈米']
 antiTSMCKeyword = ['三星', '英特爾']
 
 
-sum_category_score(marketKeyword, 'market')
-sum_category_score(TSMCKeyword, 'TSMC')
+
+# Run 1
+# sum_category_score(marketKeyword, 'market')
+# sum_category_score(TSMCKeyword, 'TSMC')
 # sum_category_score(antiTSMCKeyword, 'antiTSMC')
-#
-# sum_anti_score('TSMC', 'antiTSMC')
+
+# Run 2
+# deduct_antiTSMC_score('TSMC', 'antiTSMC')
+
+# Run 3
+# sum_all_score(['TSMCMix', 'market'])
+
+
+
+
