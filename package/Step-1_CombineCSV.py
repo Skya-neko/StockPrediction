0c7eecb21fc687e5b5cdde7515ccd7cfa87adb48
Lalalin <daby6789@gmail.com>
2022-06-17 21:44:43 +0800

    run ANN Two process A, B

--- package/Step-1_CombineCSV.py
+++ package/Step-1_CombineCSV.py
@@ -7,16 +7,24 @@ filePath = './data/'
 # recordB = sys.argv[2]
 # target = sys.argv[3]
 
+# Debug
 recordA = 'Step-0_ANNTwoResult_ProcessA.csv'
 recordB = 'Step-0_ANNTwoResult_ProcessB.csv'
 target = 'Step-0_ANNTwoResult.csv'
 
 recordADF = pd.read_csv(filePath+recordA, index_col=False)
-recrodBDF = pd.read_csv(filePath+recordB, index_col=False)
+recordBDF = pd.read_csv(filePath+recordB, index_col=False)
 targetDF = pd.read_csv(filePath+target, index_col=False)
 
-recordADF
 
-recordDf = pd.concat([recordADF, recrodBDF], axis=0).reset_index(drop=True)
+
+recordDf = pd.concat([recordADF, recordBDF], axis=0).reset_index(drop=True)
 targetDF = pd.concat([targetDF, recordDf], axis=0).reset_index(drop=True)
-targetDF.to_csv(filePath+'test.csv', encoding='big5', index=False)
+targetDF.to_csv(filePath+target, encoding='big5', index=False)
+
+recordADF = recordADF.head(0)
+recordADF.to_csv(filePath+recordA, encoding='big5', index=False)
+recordBDF = recordBDF.head(0)
+recordBDF.to_csv(filePath+recordB, encoding='big5', index=False)
+
+
