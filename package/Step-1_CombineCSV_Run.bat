5a460d3e9818c863471aff6cf3f941f810f2f31a
Lalalin <daby6789@gmail.com>
2022-06-17 23:13:16 +0800

    create Step-1_CombineCSV_Run.bat

--- package/Step-1_CombineCSV_Run.bat
+++ package/Step-1_CombineCSV_Run.bat
@@ -1,5 +1,5 @@
-cd ../
-set  project_root=%cd%
+set  project_root=D:\StockPrediction\StockPrediction\
+cd /d %project_root%
 echo Project root: %project_root%
 SET "today=%date:~0,10%"
 python   %project_root%\package\Step-1_CombineCSV.py  Step-0_ANNTwoResult_ProcessA.csv  Step-0_ANNTwoResult_ProcessB.csv  Step-0_ANNTwoResult.csv  >>  %project_root%\log\Step-1_CombineCSV_%today:/=-%.txt  2>&1
--- package/Step-1_SchtaskCreate.bat
+++ package/Step-1_SchtaskCreate.bat
@@ -1,2 +1,5 @@
-schtasks /create  /sc minute  /mo 5 /sd 2022/06/17 /st 23:00 /ed 2022/07/10 /tn run_ml /tr  D:\StockPrediction\StockPrediction\package\Step-1_CombineCSV_Run.bat
+schtasks /create  /sc minute  /mo 5 /sd 2022/06/17 /st 22:50 /ed 2022/07/10 /tn run_ml /tr  D:\StockPrediction\StockPrediction\package\Step-1_CombineCSV_Run.bat
 schtasks | find "run_ml"
+pause
+@REM If you want cmd.exe not to close to be able to remain typing, use cmd /k command at the end of the file.
+@REM cmd /k
